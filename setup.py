b4cbc378d0b78839f9277fda053cdfdad7cc5bb5
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-06-08 00:37:47 -0700

    Updated package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-learn',
+      version='0.1',
+      description='Module of basic machine learning algorithms',
+      url='https://github.com/naveenv92/py-learn',
+      author='Naveen Venkatesan',
+      author_email='naveen.venkatesan@gmail.com',
+      license='MIT',
+      packages=['pylearn'],
+      zip_safe=False)
